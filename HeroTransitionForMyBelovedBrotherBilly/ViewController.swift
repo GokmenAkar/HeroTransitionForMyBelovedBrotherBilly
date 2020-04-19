26e9cd90a90003ed95cef1b0f3d99dff78191ccf
GokmenAkar <gokmen.akar92@gmail.com>
2020-04-19 22:30:19 +0300

    - processing

--- HeroTransitionForMyBelovedBrotherBilly/ViewController.swift
+++ HeroTransitionForMyBelovedBrotherBilly/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var greatCornholioImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        greatCornholioImageView.hero.id = "16048623"
+        
+    }
+}
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? UICollectionViewCell else{
+            return UICollectionViewCell()
+        }
+        
+        return cell
     }
 }
 
