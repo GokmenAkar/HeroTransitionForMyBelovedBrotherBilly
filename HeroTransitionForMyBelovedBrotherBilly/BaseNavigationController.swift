ddff6cbb44dbfb4c1795b71d7d8722e67e0c8756
GokmenAkar <gokmen.akar92@gmail.com>
2020-04-19 21:49:12 +0300

    - sorry

---
+++ HeroTransitionForMyBelovedBrotherBilly/BaseNavigationController.swift
@@ -0,0 +1,19 @@
+//
+//  BaseNavigationController.swift
+//  HeroTransitionForMyBelovedBrotherBilly
+//
+//  Created by Developer on 19.04.2020.
+//  Copyright © 2020 Akar. All rights reserved.
+//
+
+import UIKit
+import Hero
+
+class BaseNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        hero.isEnabled = true 
+    }
+
+}
--- HeroTransitionForMyBelovedBrotherBilly/ViewController.swift
+++ HeroTransitionForMyBelovedBrotherBilly/ViewController.swift
@@ -10,11 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var greatCornholioImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
+class DesignableView: UIView {
+}
+
+extension UIView {
+    
+    @IBInspectable
+    var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+        }
+    }
+    
+    @IBInspectable
+    var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable
+    var borderColor: UIColor? {
+        get {
+            if let color = layer.borderColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.borderColor = color.cgColor
+            } else {
+                layer.borderColor = nil
+            }
+        }
+    }
+}
